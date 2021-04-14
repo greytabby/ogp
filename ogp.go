31958ed872c287589ff26d6ef09a7f05ef767709
greytabby <munchkin.coco@gmail.com>
2021-04-14 22:28:36 +0900

    Add type definition

--- ogp.go
+++ ogp.go
@@ -1,1 +1,44 @@
 package ogp
+
+type OpenGraph struct {
+	// Basic metadata
+	Title  string
+	Type   string
+	Images []Image
+	URL    string
+
+	// Optional metadata
+	Audios      []Audio
+	Videos      []Video
+	Description string
+	Determiner  string
+	Locale      Locale
+	SiteName    string
+}
+
+type Image struct {
+	URL       string
+	SecureURL string
+	Type      string
+	Width     string
+	Height    string
+	Alt       string
+}
+
+type Video struct {
+	URL       string
+	SecureURL string
+	Type      string
+	Width     string
+	Height    string
+}
+
+type Audio struct {
+	URL       string
+	SecureURL string
+	Type      string
+}
+
+type Locale struct {
+	Alternate []string
+}
